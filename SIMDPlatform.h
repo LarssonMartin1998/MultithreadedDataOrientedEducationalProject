99f210495cfd1e2eecc9641796aea50a019df249
Martin Larson <larssonmartin1998@gmail.com>
2023-08-21 11:27:37 +0200

    Shouldve been part of last commit

--- SIMDPlatform.h
+++ SIMDPlatform.h
@@ -2,9 +2,7 @@
 
 #include <cstddef>
 
-#if defined(__i386__) || defined(_M_IX86) // x86
-constexpr size_t simdWidth = 4;
-#elif defined(__x86_64__) || defined(_M_X64) // x64
+#if defined(__x86_64__) || defined(_M_X64) // x64
 constexpr size_t simdWidth = 8;
 #elif defined(__arm__) || defined(__aarch64__) // ARM
 constexpr size_t simdWidth = 4;
