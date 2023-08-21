888b63165b96b9b7a70d0fa86513426237e7070a
Martin Larson <larssonmartin1998@gmail.com>
2023-08-21 11:22:35 +0200

    Remove x86 simd target, fix a typo in the define for __X86_64__

--- SIMDPlatform.h
+++ SIMDPlatform.h
@@ -4,7 +4,7 @@
 
 #if defined(__i386__) || defined(_M_IX86) // x86
 constexpr size_t simdWidth = 4;
-#elif defined(__x86__64__) || defined(_M_X64) // x64
+#elif defined(__x86_64__) || defined(_M_X64) // x64
 constexpr size_t simdWidth = 8;
 #elif defined(__arm__) || defined(__aarch64__) // ARM
 constexpr size_t simdWidth = 4;
--- SimulateMotionJob.cpp
+++ SimulateMotionJob.cpp
@@ -3,13 +3,11 @@
 #include <iostream>
 #include <algorithm>
 
-//#if defined(__i386__) || defined(_M_IX86) // x86
-//#include <xmmintrin.h>
-//#elif defined(__x86__64__) || defined(_M_X64) // x64
+#if defined(__x86_64__) || defined(_M_X64) // x64
 #include <immintrin.h>
-//#elif defined(__arm__) || defined(__aarch64__) // ARM
-//#include <arm_neon.h>
-//#endif
+#elif defined(__arm__) || defined(__aarch64__) // ARM
+#include <arm_neon.h>
+#endif
 
 #include "SIMDPlatform.h"
 
@@ -39,80 +37,61 @@ namespace SimulateMotionJob
         size_t i = 0;
         for (; i < velocities.size(); i += simdWidth)
         {
-//#if defined(__i386__) || defined(_M_IX86)
-            /*__m128 accel = _mm_setzero_ps();
-            __m128 speed = _mm_setzero_ps();
-            __m128 direction = _mm_setzero_ps();
-            __m128 pos = _mm_setzero_ps();*/
-//#elif defined(__x86_64__) || defined(_M_X64) // x64
+#if defined(__x86_64__) || defined(_M_X64) // x64
             __m256 accel = _mm256_setzero_ps();
             __m256 speed = _mm256_setzero_ps();
             __m256 directionX = _mm256_setzero_ps();
             __m256 directionY = _mm256_setzero_ps();
             __m256 posX = _mm256_setzero_ps();
             __m256 posY = _mm256_setzero_ps();
-//#elif defined(__arm__) || defined(__aarch64__)
+#elif defined(__arm__) || defined(__aarch64__)
             /*float32x4_t accel = vdupq_n_f32(0.0f);
             float32x4_t speed = vdupq_n_f32(0.0f);
             float32x4_t direction = vdupq_n_f32(0.0f);
             float32x4_t pos = vdupq_n_f32(0.0f);*/
-//#endif
+#endif
 
             for (int j = 0; j < simdWidth; ++j)
             {
-//#if defined(__i386__) || defined(_M_IX86) // x86 architecture (SSE)
-                /*accel = _mm_set_ps(0.0f, 0.0f, 0.0f, physics[i + j].acceleration);
-                speed = _mm_set_ps(0.0f, 0.0f, 0.0f, velocities[i + j].speed);
-                direction = _mm_set_ps(0.0f, 0.0f, 0.0f, velocities[i + j].direction);
-                pos = _mm_set_ps(0.0f, 0.0f, 0.0f, positions[i + j].pos);*/
-//#elif defined(__x86_64__) || defined(_M_X64) // x64 architecture (AVX)
+#if defined(__x86_64__) || defined(_M_X64) // x64 architecture (AVX)
                 accel = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, physics[i + j].acceleration);
                 speed = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, velocities[i + j].speed);
                 directionX = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, velocities[i + j].direction.x);
                 directionY = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, velocities[i + j].direction.y);
                 posX = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, positions[i + j].pos.x);
                 posY = _mm256_set_ps(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, positions[i + j].pos.y);
-/*#elif defined(__arm__) || defined(__aarch64__) // ARM architecture (NEON)
 #elif defined(__arm__) || defined(__aarch64__) // ARM architecture (NEON)
                 accel = vsetq_lane_f32(physics[i + j].acceleration, accel, j);
                 speed = vsetq_lane_f32(velocities[i + j].speed, speed, j);
                 direction = vsetq_lane_f32(velocities[i + j].direction, direction, j);
                 pos = vsetq_lane_f32(positions[i + j].pos, pos, j);
-#endif*/
+#endif
             }
 
             // Perform SIMD operations
-//#if defined(__i386__) || defined(_M_IX86) // x86
-            /*accel = _mm_sub_ps(accel, _mm_set1_ps(gravityDelta));
-            speed = _mm_max_ps(speed, _mm_add_ps(speed, _mm_mul_ps(accel, _mm_set1_ps(deltaTime))));
-            pos = _mm_add_ps(pos, _mm_mul_ps(direction, _mm_mul_ps(speed, _mm_set1_ps(deltaTime))));*/
-//#elif defined(__x86_64__) || defined(_M_X64) // x64
+#if defined(__x86_64__) || defined(_M_X64) // x64
             accel = _mm256_sub_ps(accel, _mm256_set1_ps(gravityDelta));
             speed = _mm256_max_ps(speed, _mm256_add_ps(speed, _mm256_mul_ps(accel, _mm256_set1_ps(deltaTime))));
             posX = _mm256_add_ps(posX, _mm256_mul_ps(directionX, _mm256_mul_ps(speed, _mm256_set1_ps(deltaTime))));
             posY = _mm256_add_ps(posY, _mm256_mul_ps(directionY, _mm256_mul_ps(speed, _mm256_set1_ps(deltaTime))));
-//#elif defined(__arm__) || defined(__aarch64__) // ARM
+#elif defined(__arm__) || defined(__aarch64__) // ARM
             /*accel = vsubq_f32(accel, vdupq_n_f32(gravityDelta));
             speed = vmaxq_f32(speed, vmlaq_n_f32(speed, accel, deltaTime));
             pos = vmlaq_n_f32(pos, direction, deltaTime);*/
-//#endif
+#endif
 
             for (int j = 0; j < simdWidth; ++j)
             {
-/*#if defined(__i386__) || defined(_M_IX86) // x86
-                positions[i + j].pos.x = _mm_cvtss_f32(posX);
-                velocities[i + j].speed = _mm_cvtss_f32(speed);
-                physics[i + j].acceleration = _mm_cvtss_f32(accel);*/
-//#elif defined(__x86_64__) || defined(_M_X64) // x64
+#if defined(__x86_64__) || defined(_M_X64) // x64
                 positions[i + j].pos.x = _mm256_cvtss_f32(posX);
                 positions[i + j].pos.y = _mm256_cvtss_f32(posY);
                 velocities[i + j].speed = _mm256_cvtss_f32(speed);
                 physics[i + j].acceleration = _mm256_cvtss_f32(accel);
-/*#elif defined(__arm__) || defined(__aarch64__) // ARM
+#elif defined(__arm__) || defined(__aarch64__) // ARM
                 positions[i + j].pos = vgetq_lane_f32(pos, j);
                 velocities[i + j].speed = vgetq_lane_f32(speed, j);
                 physics[i + j].acceleration = vgetq_lane_f32(accel, j);
-#endif*/
+#endif
             }
         }
 
